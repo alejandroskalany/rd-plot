e47e3209f0a82b9666940b7751366fed1cdd3c3e
Alejandro Skalany <alskalany@gmail.com>
2022-01-11 18:02:17 -0600

    commit

--- main.py
+++ main.py
@@ -5,61 +5,185 @@
 # brief      : log plot reading utility
 # updates by : 
 # ============================================================
-from tkinter import filedialog
-import tkinter as tk
-from tkinter import *
-from tkinter import ttk
-#from tkinter import font
-from PIL import Image
+
+##TO DO -> support for log, support for black color curves
+
+import cv2
 import numpy as np
+from tkinter import ttk
+from tkinter import filedialog
+from tkinter import *
+import tkinter as tk 
+from tkinter import Tk
+from numpy.core.defchararray import upper
+import ctypes
+from numpy import asarray
+import PIL
+from PIL import Image
 import pandas as pd
-import os
 
-debug = False
+user_depth = 0
+user_color = 0
+user_xmin = 0
+user_xmax = 0
+user_ymin = 0
+user_ymax = 0
+edges = np.zeros((2,2),np.int)
+counter = 0
 
 
-# ============================================================
-def using_np():
-    global filename, progress_output_text
-    global min_value, max_value, min_depth_val, max_depth_val
-    global head_tail, im, width, depth
-    global data, red, green, blue, alpha
+def get_edges():
+    global col1, col2, image
 
-    gthres = 20
-    bthres = 150
-    dthres = 40
-    bgr = 0
-    bgg = 0
-    bgb = 0
-    green_areas  = (red > (green - gthres)) & (blue > (green - gthres))
-    grey_areas   = (red == green) & (red == blue) & (blue == green) # works
-    bright_areas = (red > bthres) & (green > bthres) & (blue > bthres) # works
-    dark_areas   = (red < dthres) & (green < dthres) & (blue < dthres)
+    Tk().withdraw() 
+    filename = filedialog.askopenfilename()
+    im = cv2.imread(filename)
 
-    data[..., :-1][bright_areas.T] = (bgr, bgg, bgb) # Transpose back needed
-    data[..., :-1][grey_areas.T]   = (bgr, bgg, bgb) # Transpose back needed
-    data[..., :-1][dark_areas.T]   = (bgr, bgg, bgb) # Transpose back needed
-    data[..., :-1][green_areas.T]  = (bgr, bgg, bgb) # Transpose back needed
+    #resize to fit window
+    image = cv2.resize(im, (1000,800))
 
-    im2 = Image.fromarray(data)
-    if debug:
-        im2.show()
-    rgb_im = im2.convert('RGB')
-    rgb_im.save('processed.jpg')
+    #dialog with instructions
+    ctypes.windll.user32.MessageBoxW(0, "Please select your desired track's left and right boundaries", "Color Curve Digitizer", 1)
 
-    # calculate xvals from image
+    while True:
+
+        for x in range (0,2):
+                cv2.circle(image, (edges[x][0],edges[x][1]),2,(0,0,0),cv2.FILLED)
+
+        if counter == 2:
+        #collect first and last columns
+            col1 = (edges[0][0])
+            col2 = (edges[1][0])
+
+            cv2.destroyAllWindows()
+
+            break #continue once two edges are detected
+
+        cv2.imshow("Image", image)
+        cv2.setMouseCallback("Image", findEdges)
+        cv2.waitKey(1)
+
+
+def user_input_win():
+    global user_color, user_xmin, user_xmax, user_ymin, user_ymax
+    global col1, col2, color_entry, r, color
+    global xmax_entry, xmin_entry, ymax_entry, ymin_entry
+
+    root = tk.Tk()
+    root.title("Color Curve Digitizer")
+
+    #center window
+    w = 250
+    h = 300
+    ws = root.winfo_screenwidth()
+    hs = root.winfo_screenheight()
+    x = (ws/2) - (w/2)
+    y = (hs/2) - (h/2)
+    root.geometry('%dx%d+%d+%d' % (w,h,x,y))
+
+    depth = tk.StringVar()
+    xmin = tk.StringVar()
+    xmax = tk.StringVar()
+    ymin = tk.StringVar()
+    ymax = tk.StringVar()
+
+    #input frame
+    entry = ttk.Frame(root)
+    entry.pack(padx=2, pady=0, fill='x', expand=True)
+
+    xmin_label = ttk.Label(entry, text = "X Min").pack()
+    xmin_entry = ttk.Entry(entry, textvariable = xmin, justify = 'center')
+    xmin_entry.pack()
+        
+    xmax_label = ttk.Label(entry, text = "X Max").pack()
+    xmax_entry = ttk.Entry(entry, textvariable = xmax, justify='center')
+    xmax_entry.pack()
+
+    ymin_label = ttk.Label(entry, text = "Y Min").pack()
+    ymin_entry = ttk.Entry(entry, textvariable = ymin, justify = 'center')
+    ymin_entry.pack()
+        
+    ymax_label = ttk.Label(entry, text = "Y Max").pack()
+    ymax_entry = ttk.Entry(entry, textvariable = ymax, justify='center')
+    ymax_entry.pack()
+
+    colors = [('Green', 'G'),('Red', 'R'),('Blue', 'B'),('Black', 'BL')]
+    color_entry = tk.StringVar(entry) #pass name of frame containing radiobuttons to update button variable
+    color_entry.set(" ")
+
+    colors_label = ttk.Label(entry, text = "Track Color").pack()
+    for text, color in colors:
+        ttk.Radiobutton(entry, text=text, variable=color_entry, value=color).pack(fill='y', expand=True)
+
+    #button
+    digitize_button = ttk.Button(root, text="Digitize", command=masking)
+    digitize_button.pack(side='bottom')
+
+    root.mainloop()
+
+
+def findEdges(event,x,y, flags, params):
+    global counter, user_depth, user_color, col1, col2
+    global depth_entry, color_entry, track_slice
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        edges[counter] = x,y
+        counter = counter + 1
+
+
+def masking():
+    global user_color, edges, col1, col2, color_entry, newfile
+    global user_xmin, user_xmax, user_ymin, user_ymax, rescaled_slice
+    #create img from track selection
+    np.set_printoptions(threshold=np.inf)
+    user_color = color_entry.get()
+
+    new_image = image[0:int(8000),int(col1):int(col2)]
+
+    #convert selection to HSV for masking
+    hsv = cv2.cvtColor(new_image, cv2.COLOR_BGR2HSV)
+
+    #set color boundaries
+    upper_green = np.array([180,255,235])
+    lower_green = np.array([12,35,15])
+    upper_blue = np.array([185, 185, 244])
+    lower_blue = np.array([60, 50, 255])
+    upper_red = np.array([10, 255, 255])
+    lower_red = np.array([0, 100, 50])
+
+    #create masks
+    green_mask = cv2.inRange(hsv, lower_green, upper_green)
+    red_mask = cv2.inRange(hsv, lower_red, upper_red)
+    blue_mask = cv2.inRange(hsv, lower_blue, upper_blue) 
+        
+    #create masked images based on user color selection
+    if user_color=='G':
+        track_slice = cv2.bitwise_and(new_image, new_image, mask=green_mask)
+        
+    elif user_color=='R':
+        track_slice = cv2.bitwise_and(new_image, new_image, mask=red_mask)
+        
+    elif user_color=='B':
+        track_slice = cv2.bitwise_and(new_image, new_image, mask=blue_mask)
+
+    trackimg = cv2.imwrite("selection.png", track_slice, [cv2.IMWRITE_PNG_COMPRESSION, 0])
+
+    ## 1. calculate position of color pixels
+    im = Image.open('selection.png')
+    im2arr = np.array(im)
+    width, depth = im.size
     edge_offset = 8
-    vals = np.zeros((depth, 2))
-    for y in range(0, depth):
-        vals[y, 0] = y
+    vals = np.zeros((depth,2))
+    for y in range(0,depth):
+        vals[y,0] = y
         for x in range(edge_offset, width - edge_offset):
-            pix = im2.getpixel((x,y))
-            if (pix[0] != bgr) & (pix[0] > dthres):
+            pix = im.getpixel((x,y))
+            if (pix[0] != 0):
                 val_s = x
                 val_e = 0
                 for x2 in range(x+1, width):
-                    pix = im2.getpixel((x2, y))
-                    if pix[0] != bgr:
+                    pix = im.getpixel((x2, y))
+                    if pix[0] != 0:
                         continue
                     else:
                         val_e = x2 - 1
@@ -68,13 +192,7 @@ def using_np():
                 vals[y,1] = x_val
                 break
 
-    # delete first 4 rows
-    vals = np.delete(vals, 0, 0)
-    vals = np.delete(vals, 0, 0)
-    vals = np.delete(vals, 0, 0)
-    depth = depth - 3
-
-    # detect holes in the data
+    #detect holes in data
     for y in range(0, depth):
         if vals[y,1] == 0.0:
             if  y != depth - 1:
@@ -87,148 +205,66 @@ def using_np():
             else:
                 vals[y, 1] = vals[y - 1, 1]
 
-    # scale the height and the x values
-    min_depth_int = int(min_depth_val.get())
-    max_depth_int = int(max_depth_val.get())
-    min_value_int = int(min_value.get())
-    max_value_int = int(max_value.get())
-    num_rows, num_cols = vals.shape
-    ydelta = (max_depth_int - min_depth_int) / (num_rows - 1)
-    xdelta = (max_value_int - min_value_int) / width
-    vals[0,0] = min_depth_int
-    vals[0,1] = vals[0,1] * xdelta
-    for y in range(1, depth):
-        vals[y, 0] = vals[y - 1, 0] + ydelta
-        vals[y, 1] = vals[y, 1] * xdelta        #x scaling is not correct !!!
-
     df = pd.DataFrame(vals)
 
-    # construct output path and file name
-    # @@@ output filename is wrong... it contains jpg and should not
-    new_filename = head_tail[1].split()[0] + ".csv"
-    output_path_file = os.path.join(head_tail[0], new_filename)
-    df.to_csv(output_path_file, header=["D", "V"], float_format="%.3f", index=False)
-    progress_output_text.set("Complete")
+    #find min,max of df
+    min_x = df[0].min()
+    max_x = df[0].max()
+
+    min_y = df[1].min()
+    max_y = df[1].max()
+
+    ## get user min, max
+    user_xmax = int(xmax_entry.get())
+    user_xmin = int(xmin_entry.get())
+
+    user_ymax = int(ymax_entry.get())
+    user_ymin = int(ymin_entry.get())
+
+    ## column range max - min, user range user max - user min
+    range_x = max_x - min_x
+    range_y = max_y - min_y
+
+    user_rangex = user_xmax - user_xmin
+    user_rangey = user_ymax - user_ymin
+
+    ## expansion rate is user range/column range
+    exprate_x = (user_rangex / range_x)
+    exprate_y = (user_rangey / range_y)
+
+    ## scale
+    df[0] = df[0].apply(lambda x: (x - min_x)*exprate_x + user_xmin)
+    df[1] = df[1].apply(lambda x: (x - min_y)*exprate_y + user_ymin)
+
+    ## 10. save new array as csv
+    df.to_csv("processed.csv", header=["D", "V"], float_format="%.3f", index=False)
+
+    quit_window()
 
 
-# =============================================
-def tk_start():
-    global filename, root, progress_output_text
-    global min_value, max_value, min_depth_val, max_depth_val
-    global head_tail, im, width, depth
-    global data, red, green, blue, alpha
-
-    content = ttk.Frame(root)
-    content.grid(column=0, row=0)
-
-    root.title("Color Curve Digitizer")
-    root.maxsize(600, 600)
-
-    style_path = ttk.Style()
-    style_path.configure("style_path.TLabel", background='lightgreen')
-    path_label = ttk.Label(content, text="      ")
-    path_label['style'] = "style_path.TLabel"
-    path_label.grid(row=0, column=0, columnspan=3)
-
-    style_file = ttk.Style()
-    style_file.configure("style_file.TLabel", background='green', foreground='white')
-    file_label = ttk.Label(content, text="      ")
-    file_label['style'] = "style_file.TLabel"
-    file_label.grid(row=1, column=0, columnspan=3)
-
-    min_value = StringVar(value="0")
-    min_entry = ttk.Entry(content, textvariable=min_value, justify='center')
-    min_entry.grid(row=2, column=0)
-
-    min_label = ttk.Label(content, text="Min Value")
-    min_label.grid(row=2, column=1)
-
-    max_value = StringVar(value="100")
-    max_entry = ttk.Entry(content, textvariable=max_value, justify='center')
-    max_entry.grid(row=3, column=0)
-
-    max_label = ttk.Label(content, text="Max Value")        # , fg="green"
-    max_label.grid(row=3, column=1)
-
-    min_depth_val = StringVar(value="1000")
-    min_depth = ttk.Entry(content, textvariable=min_depth_val, justify='center')
-    min_depth.grid(row=4, column=0)
-
-    min_depth_label = ttk.Label(content, text="Min Height")        # , fg="green"
-    min_depth_label.grid(row=4, column=1)
-
-    max_depth_val = StringVar(value="2000")
-    max_depth = ttk.Entry(content, textvariable=max_depth_val, justify='center')
-    max_depth.grid(row=5, column=0)
-
-    max_depth_label = ttk.Label(content, text="Max Height")        # , fg="green"
-    max_depth_label.grid(row=5, column=1)
-
-    # @@@ linear should be selected but for some reason it is not...
-    var = IntVar()
-    var.set(1)
-    x_axis_label = ttk.Label(content, text="X-Axis type")
-    rb1 = ttk.Radiobutton(content, text="Linear", variable=var, value=0)
-    rb2 = ttk.Radiobutton(content, text="Log", variable=var, value=1)
-    rb1.grid(row=6, column=0)
-    rb2.grid(row=6, column=1)
-    x_axis_label.grid(row=6, column=2)
-
-    progress_output_text = StringVar(value='Ready')
-    progress_output = ttk.Entry(content, textvariable=progress_output_text, justify='center')
-    progress_output.grid(row=7, column=0, columnspan=2)
-    progress_output["state"] = tk.DISABLED
-
-    execute_button = ttk.Button(content, text="Execute", command=using_np)       # fg="blue",
-    execute_button.grid(row=8, column=0)
-
-    quit_button = ttk.Button(content, text="Quit", command=root.destroy)     # fg="red",
-    quit_button.grid(row=8, column=1)
-
-    head_tail = os.path.split(filename)
-    path_label["text"] = head_tail[0]
-    file_label["text"] = head_tail[1]
-
-    # open the file
-    im = Image.open(filename)
-    im = im.convert('RGBA')
-    width, depth = im.size
-
-    # convert the file
-    data = np.array(im)  # "data" is a height x width x 4 numpy array
-    red, green, blue, alpha = data.T  # Temporarily unpack the bands for readability
-    print("data shape : ", data.shape)
-
-    # find colors in file
-    # choose a height, say 25 % of the height
-    pdepth = int((depth * 25) / 100)
-    color_choose = data[pdepth,:,:]
-    print("color shape : ", color_choose.shape)
-    colorbar = color_choose
-    for i in range(0, 20):
-        colorbar = np.append(colorbar, color_choose)
-
-    colorbar = np.append(colorbar, color_choose).reshape((22,width,4))
-    print("colorbar shape     : ", colorbar.shape)
-
-    im = Image.fromarray(colorbar)
-    im.save("colorbar.png")
-    # @@@ the "track in question always has a min and a max on the page
-    # @@@ the application should allow the user to select the lefthand and righthand boundaries of the track
-
-    # @@@ the user should be able to select the color of the track
-
-
-# ============================================================
-if __name__ == '__main__':
-    # get the filename
+def quit_window():
     root = tk.Tk()
-    root.withdraw()
-    filename = filedialog.askopenfilename()
-    root.destroy()
+    root.title("Color Curve Digitizer")
 
-    # launch the app
-    root = Tk()
-    tk_start()
+    #center window
+    newfile = "'processed.csv'"
+    w = 400
+    h = 50
+    ws = root.winfo_screenwidth()
+    hs = root.winfo_screenheight()
+    x = (ws/2) - (w/2)
+    y = (hs/2) - (h/2)
+    root.geometry('%dx%d+%d+%d' % (w,h,x,y))
+    
+    depth_label = ttk.Label(root, text = "Your selection has been processed and saved as {}.".format(newfile), anchor=tk.CENTER)
+    depth_label.pack(padx=5, pady=5, fill='x', expand=True)
+
+    Button(root, text="Done", command=quit).pack()
 
     root.mainloop()
+    
+
+if __name__ == '__main__':
+
+    get_edges()
+    user_input_win()
